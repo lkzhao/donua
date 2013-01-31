e84ae483cfa680bed81bee015c511e82427cb2cd
luke <lzhaoyilun@gmail.com>
2013-01-31 12:40:23 -0500

    Initial Commit

---
+++ donua/MAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MAppDelegate.h
+//  donua
+//
+//  Created by Luke Zhao on 2013-01-31.
+//  Copyright (c) 2013 mochaka. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ donua/MAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  MAppDelegate.m
+//  donua
+//
+//  Created by Luke Zhao on 2013-01-31.
+//  Copyright (c) 2013 mochaka. All rights reserved.
+//
+
+#import "MAppDelegate.h"
+
+@implementation MAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
